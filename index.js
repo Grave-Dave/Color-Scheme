99645d0da19aba19e78378459afee9e75c8747ee
Dawid <dawgrab1@gmail.com>
2023-01-10 12:37:58 +0100

    modal improved

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+function start() {
+	let i = 0;
+	const modalShadow = document.querySelector('.modal-shadow');
+	const closeBtn = document.querySelector('.close-btn');
+	const liItem = document.querySelectorAll('li');
+	closeBtn.addEventListener('click', handleModal);
+
+	function handleModal() {
+		if (i === 0) {
+			modalShadow.classList.remove('hidden');
+		}
+		if (i > 0) {
+			liItem[i - 1].classList.remove('active');
+		}
+		if (i === 2) {
+			closeBtn.classList.add('color-btn');
+			closeBtn.textContent = 'Close';
+			closeBtn.removeEventListener('click', handleModal);
+			handleApp();
+		}
+		liItem[i].classList.add('active');
+		i++;
+		hadleAnimation(i)
+	}
+}
+
 const hexValues = document.querySelectorAll('.color-hex');
 hexValues.forEach(hex =>
 	hex.addEventListener('click', () => {
@@ -7,44 +33,52 @@ hexValues.forEach(hex =>
 	})
 );
 
-function hadleAnimation(p){
-	document.querySelector(`.${p}`).classList.toggle('active')
+function hadleAnimation(p) {
+	console.log(p);
+	document.querySelector(`.p${p}`).classList.add('active');
+	p > 0 && document.querySelector(`.p${p-1}`).classList.remove('active');
 }
 
-document.querySelectorAll('button').forEach(button=>button.addEventListener('click', () => {
-	document.querySelectorAll('.to-hide').forEach(el=>el.classList.add('hidden'))
-	const colorValue = document.querySelector('input').value.slice(1);
-	const modeValue = document.querySelector('select').value.toLowerCase();
-	document.querySelectorAll('.color').forEach(div => {
-		div.childNodes[0] ? div.childNodes[0].remove() : '';
-	});
-
-	fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${modeValue}&count=5`)
-		.then(res => res.json())
-		.then(data => {
-			let i = 0;
-			document.querySelectorAll('h2').forEach(el => {
-				el.innerHTML = data.colors[i].hex.value;
-				i++;
-			});
-			let j = 0;
+function handleApp() {
+	document.querySelectorAll('.color-btn').forEach(button =>
+		button.addEventListener('click', () => {
+			document.querySelectorAll('.to-hide').forEach(el => el.classList.add('hidden'));
+			const colorValue = document.querySelector('input').value.slice(1);
+			const modeValue = document.querySelector('select').value.toLowerCase();
 			document.querySelectorAll('.color').forEach(div => {
-				div.style.backgroundColor = data.colors[j].hex.value;
-				j++;
+				div.childNodes[0] ? div.childNodes[0].remove() : '';
 			});
 
-			let k = -1;
-			const arr = new Array(data.colors.length).fill(0).map(el => {
-				k++;
-				return data.colors[k].name.value;
-			});
-			let l = 0;
-			for (el of arr) {
-				const p = document.createElement('p');
-				p.classList.add('description');
-				p.textContent = el;
-				document.querySelectorAll('.color')[l].append(p);
-				l++;
-			}
-		});
-}));
+			fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${modeValue}&count=5`)
+				.then(res => res.json())
+				.then(data => {
+					let i = 0;
+					document.querySelectorAll('h2').forEach(el => {
+						el.innerHTML = data.colors[i].hex.value;
+						i++;
+					});
+					let j = 0;
+					document.querySelectorAll('.color').forEach(div => {
+						div.style.backgroundColor = data.colors[j].hex.value;
+						j++;
+					});
+
+					let k = -1;
+					const arr = new Array(data.colors.length).fill(0).map(el => {
+						k++;
+						return data.colors[k].name.value;
+					});
+					let l = 0;
+					for (el of arr) {
+						const p = document.createElement('p');
+						p.classList.add('description');
+						p.textContent = el;
+						document.querySelectorAll('.color')[l].append(p);
+						l++;
+					}
+				});
+		})
+	);
+}
+
+start();
